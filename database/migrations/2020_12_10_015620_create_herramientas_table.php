16f1d08733c6109e938aaffecc5c349deee2c644
Mauricio221911210 <al221911210@gmail.com>
2020-12-09 20:02:23 -0600

    migraciones

---
+++ database/migrations/2020_12_10_015620_create_herramientas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHerramientasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->increments('id_he');
+            $table->String('nombre');
+            $table->double('costo');
+            $table->double('descuentos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('herramientas');
+    }
+}
